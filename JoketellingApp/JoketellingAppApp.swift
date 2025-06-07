4b82c4c1f27fbbd6f249c7ba55f4b213611ca7cf
Danil Anikin <mart4r.clearlist@gmail.com>
2025-06-07 23:33:38 +1000

    Initial Commit

---
+++ JoketellingApp/JoketellingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoketellingAppApp.swift
+//  JoketellingApp
+//
+//  Created by Данил Аникин on 07.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct JoketellingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
